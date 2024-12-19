4dceeb628c7fdafff2a8a172ed1bf0dd2e8bb1eb
Minsu Kang <01minsu.kang@gmail.com>
2024-12-19 17:53:14 +0900

    안 쓰이는 import 제거

--- flask/word_cloud/word_cloud.py
+++ flask/word_cloud/word_cloud.py
@@ -12,7 +12,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
